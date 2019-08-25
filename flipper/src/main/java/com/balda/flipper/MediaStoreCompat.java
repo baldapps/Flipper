8cd5b818986c47eb6f9d7893f664b53d078f2ab2
baldapps <playappassistance@gmail.com>
2019-08-25 12:29:44 +0200

    Improved MediaStoreCompat API

--- flipper/src/main/java/com/balda/flipper/MediaStoreCompat.java
+++ flipper/src/main/java/com/balda/flipper/MediaStoreCompat.java
@@ -33,11 +33,15 @@ import android.provider.MediaStore;
 import android.text.TextUtils;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
+import androidx.annotation.IntDef;
 import androidx.annotation.NonNull;
+
+
+import static com.balda.flipper.MediaStoreCompat.Status.DELETE;
 
 @SuppressWarnings("unused")
 public class MediaStoreCompat {
@@ -45,10 +49,19 @@ public class MediaStoreCompat {
     private Context context;
     private String folder;
     private String subFolder;
-    private MediaStoreCompatListener listener;
+
+    @IntDef({Status.DONE, DELETE, Status.ASYNC})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface Status {
+        int DONE = 0;
+        int DELETE = 1;
+        int ASYNC = 2;
+    }
+
 
     public interface MediaStoreCompatListener {
-        void onSave(OutputStream os, Uri result);
+        @Status
+        int onResult(Uri result);
     }
 
     /**
@@ -56,12 +69,10 @@ public class MediaStoreCompat {
      *
      * @param c The content
      * @param f One of Directory defined in Environment class
-     * @param l The operation listener
      */
-    public MediaStoreCompat(@NonNull Context c, @NonNull String f, @NonNull MediaStoreCompatListener l) {
+    public MediaStoreCompat(@NonNull Context c, @NonNull String f) {
         context = c;
         folder = f;
-        listener = l;
     }
 
     /**
@@ -73,42 +84,70 @@ public class MediaStoreCompat {
         subFolder = sub;
     }
 
-    public void saveImage(@NonNull FileDescription description) throws MediaStoreCompatException {
-        saveMedia(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, description);
+    public void saveImage(@NonNull FileDescription description,
+                          @NonNull MediaStoreCompatListener l) throws MediaStoreCompatException {
+        saveMedia(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, description, l);
     }
 
-    public void saveVideo(@NonNull FileDescription description) throws MediaStoreCompatException {
-        saveMedia(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, description);
+    public void saveVideo(@NonNull FileDescription description,
+                          @NonNull MediaStoreCompatListener l) throws MediaStoreCompatException {
+        saveMedia(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, description, l);
     }
 
-    public void saveAudio(@NonNull FileDescription description) throws MediaStoreCompatException {
-        saveMedia(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, description);
+    public void saveAudio(@NonNull FileDescription description,
+                          @NonNull MediaStoreCompatListener l) throws MediaStoreCompatException {
+        saveMedia(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, description, l);
     }
 
-    private void saveMedia(Uri uri, FileDescription description) throws MediaStoreCompatException {
+    /**
+     * Dispose the result uri
+     *
+     * @param uri The uri returned in onResult callback
+     */
+    public void dispose(@NonNull Uri uri) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ContentResolver resolver = context.getContentResolver();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put(MediaStore.MediaColumns.IS_PENDING, 0);
+            try {
+                resolver.update(uri, contentValues, null, null);
+            } catch (Exception ignored) {
+            }
+        }
+    }
+
+    private void saveMedia(Uri uri, FileDescription description,
+                           @NonNull MediaStoreCompatListener l) throws MediaStoreCompatException {
         ContentResolver resolver = context.getContentResolver();
         OutputStream os;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
             ContentValues contentValues = new ContentValues();
             contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, description.getName());
             contentValues.put(MediaStore.MediaColumns.MIME_TYPE, description.getMime());
+            contentValues.put(MediaStore.MediaColumns.IS_PENDING, 1);
             String imagesDir = folder;
             if (!TextUtils.isEmpty(subFolder)) {
                 imagesDir += File.separator + subFolder;
             }
             contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, imagesDir);
             contentValues.put(MediaStore.MediaColumns.OWNER_PACKAGE_NAME, context.getPackageName());
-            Uri imageUri = resolver.insert(uri, contentValues);
-            if (imageUri == null)
+            Uri uriRes = resolver.insert(uri, contentValues);
+            if (uriRes == null)
                 throw new MediaStoreCompatException(new RemoteException());
-            try {
-                os = resolver.openOutputStream(imageUri);
-            } catch (FileNotFoundException e) {
-                throw new MediaStoreCompatException(e);
+            int res = l.onResult(uriRes);
+            switch (res) {
+                case Status.DELETE:
+                    resolver.delete(uriRes, null, null);
+                    break;
+                default:
+                case Status.DONE:
+                    contentValues.clear();
+                    contentValues.put(MediaStore.MediaColumns.IS_PENDING, 0);
+                    resolver.update(uriRes, contentValues, null, null);
+                    break;
+                case Status.ASYNC:
+                    break;
             }
-            if (os == null)
-                throw new MediaStoreCompatException(new FileNotFoundException());
-            listener.onSave(os, imageUri);
         } else {
             File extDir = Environment.getExternalStoragePublicDirectory(folder);
 
@@ -131,13 +170,18 @@ public class MediaStoreCompat {
                 //noinspection ResultOfMethodCallIgnored
                 path.mkdir();
             }
-            File image = new File(path, description.getFullName());
-            try {
-                os = new FileOutputStream(image);
-            } catch (FileNotFoundException e) {
-                throw new MediaStoreCompatException(e);
+            File f = new File(path, description.getFullName());
+            int res = l.onResult(Uri.fromFile(f));
+            switch (res) {
+                case Status.DELETE:
+                    //noinspection ResultOfMethodCallIgnored
+                    f.delete();
+                    break;
+                default:
+                case Status.DONE:
+                case Status.ASYNC:
+                    break;
             }
-            listener.onSave(os, Uri.fromFile(image));
         }
     }
 }
